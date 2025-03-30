77e917ea4a8261545e177b0b9138849662c15bbb
IH0kN3m <ih0kn3m@gmail.com>
2025-03-30 19:57:22 +0300

    Initial Commit

---
+++ SuperChargedLoop/SuperChargedLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperChargedLoopApp.swift
+//  SuperChargedLoop
+//
+//  Created by Michael Myers on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SuperChargedLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
